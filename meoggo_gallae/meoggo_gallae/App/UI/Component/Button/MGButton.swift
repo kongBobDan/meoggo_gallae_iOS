7166b1cdf4e02f3418c6fa2a8314872771d0e83d
di351 <di35180@gmail.com>
2025-04-19 21:14:04 +0900

    2025.04.19/버튼 컴포넌트 수정

--- meoggo_gallae/meoggo_gallae/App/UI/Component/Button/MGButton.swift
+++ meoggo_gallae/meoggo_gallae/App/UI/Component/Button/MGButton.swift
@@ -2,85 +2,51 @@
 //  MGButton.swift
 //  meoggo_gallae
 //
-//  Created by 이다경 on 4/18/25.
+//  Created by 이다경 on 4/19/25.
 //
 
 import SwiftUI
 
-enum MGButtonType {
-    case enabled, focused, pressed
-}
+// MARK: - PLEPButton View
 
-enum MGButtonSize {
-    case small, medium
-}
-
-enum MGButtonColor {
-    case light, fill, dark
-}
-
-// MARK: - Style Resolver
-
-struct MGButtonStyle {
+struct MGButton: View {
+    let title: String
     let type: MGButtonType
     let size: MGButtonSize
     let color: MGButtonColor
+    let ricon: String? = nil
+    let licon: String? = nil
 
-    var height: CGFloat {
-        switch size {
-        case .medium: return 54
-        case .small: return 44
-        }
-    }
+    let action: () -> Void
 
-    var textStyle: TextStyle {
-        switch size {
-        case .medium: return TextStyle.button.text
-        case .small: return TextStyle.button.smalltext
-        }
-    }
-
-    var textColor: Color {
-        if color == .light {
-            return .s["gray"]
-        } else {
-            return .b[0]
-        }
-    }
-
-    var backgroundColor: Color {
-        switch type {
-        case .filled:
-            switch (enabled, color) {
-            case (true, .purple): return .p[500]
-            case (false, .purple): return .p[300]
-            case (true, .dark): return .b[600]
-            case (false, .dark): return .b[600].opacity(0.6)
-            }
-        case .outlined:
-            return .g[0]
-        case .neutral:
-            switch (enabled, color) {
-            case (true, .purple): return .g[100]
-            case (false, .purple): return .g[100].opacity(0.6)
-            case (true, .dark): return .p[50]
-            case (false, .dark): return .p[50].opacity(0.6)
+    var body: some View {
+        Button(action: action) {
+            if size == .medium {
+                HStack {
+                    if let left = licon {
+                        Image(systemName: left)
+                            .resizable()
+                            .frame(width: 24, height: 24)
+                    }
+                    Text(title)
+                    if let right = ricon {
+                        Image(systemName: right)
+                            .resizable()
+                            .frame(width: 24, height: 24)
+                    }
+                }
+            } else {
+                Text(title)
             }
         }
+        .textStyle(style.textStyle)
+        .frame(maxWidth: .infinity, minHeight: style.height)
+        .foregroundColor(style.textColor)
+        .background(style.backgroundColor)
+        .cornerRadius(6)
     }
 
-    var borderColor: Color {
-        guard type == .outlined else { return .clear }
-
-        switch (enabled, color) {
-        case (true, .purple): return .g[200]
-        case (false, .purple): return .g[200].opacity(0.6)
-        case (true, .dark): return .p[500]
-        case (false, .dark): return .p[500].opacity(0.6)
-        }
-    }
-
-    var borderWidth: CGFloat {
-        return type == .outlined ? 1 : 0
+    private var style: MGButtonStyle {
+        MGButtonStyle(type: type, size: size, color: color)
     }
 }
