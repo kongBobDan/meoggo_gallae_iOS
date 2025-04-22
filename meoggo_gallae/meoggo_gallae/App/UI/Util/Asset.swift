63cbd45cd0149b7f414572c325f4c1d3054302e8
di351 <di35180@gmail.com>
2025-04-22 12:19:46 +0900

    2025.04.22/이미지 추가 및 이미지 명 설정

---
+++ meoggo_gallae/meoggo_gallae/App/UI/Util/Asset.swift
@@ -0,0 +1,23 @@
+//
+//  Asset.swift
+//  meoggo_gallae
+//
+//  Created by 이다경 on 4/22/25.
+//
+
+import SwiftUI
+
+class Asset {
+    struct Onboarding {
+        static let second = "SecondImage"
+        static let third = "ThirdImage"
+        struct fourth {
+            static let ver1 = "Fourth1verImage"
+            static let ver2 = "Fourth2verImage"
+        }
+    }
+    struct Logo {
+        static let font = "LogoFont"
+        static let icon = "LogoIcon"
+    }
+}
