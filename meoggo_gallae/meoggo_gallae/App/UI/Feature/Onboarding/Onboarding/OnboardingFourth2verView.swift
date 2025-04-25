02406573bd77f11d6a3ccfeb4c3585f1755e420b
di351 <di35180@gmail.com>
2025-04-25 22:12:22 +0900

    2025.04.25/일부 svg 이미지 수정2

--- meoggo_gallae/meoggo_gallae/App/UI/Feature/Onboarding/Onboarding/OnboardingFourth2verView.swift
+++ meoggo_gallae/meoggo_gallae/App/UI/Feature/Onboarding/Onboarding/OnboardingFourth2verView.swift
@@ -9,7 +9,30 @@ import SwiftUI
 
 struct OnboardingFourth2verView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image(Asset.Onboarding.fourth.ver2)
+                .resizable()
+                .frame(width: 491, height: 691)
+                .offset(y: 170)
+            VStack {
+                Spacer()
+                    .frame(height: 100)
+                Text("너무하네.. 정말...")
+                    .textStyle(TextStyle.title1.bold)
+                Text("그래도 먹고갈래는 해줄꺼지?")
+                    .textStyle(TextStyle.title1.bold)
+                Spacer()
+                MGButton (
+                    title: "당연하지!",
+                    type: .enabled,
+                    size: .medium,
+                    color: .dark,
+                    action: {}
+                )
+            }
+            .frame(width: 333)
+            .padding(.bottom)
+        }
     }
 }
 
